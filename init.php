56c8b42f545f8f449bffe4d032e88ec6c3945407
lecramed0 <lecramed@gmail.com>
2017-06-13 18:12:51 -0230

    updated links to work locally

--- init.php
+++ init.php
@@ -4,16 +4,16 @@ ini_set('display_errors', 0); //do not display errors
 ini_set('log_errors', 1); //do log errors
 
 //defining constants
-define("SITEURL", 'http://photo_site.com'); //can use as the beginning part of hrefs
-define("SITEINDEX", 'http://photo_site.com/index.php');
-define('ROOTDIRECTORY', '/home/photo_site/public_html'); //define the root directory (use for php includes only, otherwise use www links)
+define("SITEURL", '/simple_photo_site'); //can use as the beginning part of hrefs
+define("SITEINDEX", '/simple_photo_site/index.php');
+define('ROOTDIRECTORY', 'C:\xampp\htdocs\simple_photo_site'); //define the root directory (use for php includes only, otherwise use www links)
 
 $current_date = new DateTime();
 $current_date_string = $current_date->format('Y-m-d H:i:s');
 $current_year_string = $current_date->format('Y');
 $current_month_string = $current_date->format('m');
 
-require ROOTDIRECTORY.'/functions/functions.php';
+//require ROOTDIRECTORY.'/functions/functions.php';
 
 //initializing $errors as an array
 $errors = array();
